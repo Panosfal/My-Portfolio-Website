c576b3ed7f382078bea261f8ac1b5ed71a9b16ab
Panagiotis Faltsioni <115566190+Panosfal@users.noreply.github.com>
2023-08-30 12:43:21 +0300

    Add files via upload
    -added animated icon that prompts user to scroll down
    -corrected menu buttons with proper icons and sizes
    -created a custom icon for the browser tabs
    -styled the form-submit button for its hovered and focused state

--- nav.js
+++ nav.js
@@ -24,10 +24,18 @@ navToggle.addEventListener('click', () =>{
 
 })
 
-// NAVBAR CLICKS CLOSE THE NAVBAR
-    
-
-
+// ANIMATED DOWN ARROW ICON 
+$(document).ready(function() {
+  
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+      if (scroll >= 1) {
+        $('.arrow').addClass('fade');
+      } else{
+        $('.arrow').removeClass('fade');
+      }
+    })
+  });
 /////////////// FUNCTIONS ////////////////////
 
 //OPEN NAVIGATION
